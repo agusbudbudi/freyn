9091f245e033754f351b7f88f96b507956f3afa7
Agus Budiman <agus.budiman@diricare.com>
2025-10-04 19:09:54 +0700

    fix error deployment

--- next.config.js
+++ next.config.js
@@ -6,12 +6,6 @@ const nextConfig = {
   },
   // Enable SWC minification for better performance
   swcMinify: true,
-  // Configure environment variables
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
 };
 
 module.exports = nextConfig;
